f945b43fa6c4821298178f8961569d0b359f5aeb
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2021-08-24 14:13:39 -0400

    Minor bug fixes, refactor, & UI updates

--- src/index.js
+++ src/index.js
@@ -2,26 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Box extends React.Component {
-
-  render() {
-    return (
-      <div className={"box" +
-          (this.props.top ? " top" + this.props.top : "") +
-          (this.props.bottom ? " bottom" + this.props.bottom : "") +
-          (this.props.left ? " left" + this.props.left : "") +
-          (this.props.right ? " right" + this.props.right : "") +
-          (this.props.winner ? " winner" + this.props.winner : "")}
-          onClick={this.props.onClick}>
-        <span className='winner'>{this.props.winner || "_"}</span>
-        <div className='topLeft'></div>
-        <div className='topRight'></div>
-        <div className='bottomLeft'></div>
-        <div className='bottomRight'></div>
-      </div>
-    );
-  }
-}
+const Box = (props) =>
+  <div className={"box" +
+      (props.top ? " top" + props.top : "") +
+      (props.bottom ? " bottom" + props.bottom : "") +
+      (props.left ? " left" + props.left : "") +
+      (props.right ? " right" + props.right : "") +
+      (props.winner ? " winner" + props.winner : "")}
+      onClick={props.onClick}>
+    <span className='winner'>{props.winner || "_"}</span>
+    <div className='topLeft'></div>
+    <div className='topRight'></div>
+    <div className='bottomLeft'></div>
+    <div className='bottomRight'></div>
+  </div>
 
 
 
@@ -48,12 +42,25 @@ class Board extends React.Component {
     
   }
 
-  render() {
+  gameStatus() {
+    if ( this.state.scores["A"] + this.state.scores["B"] < this.state.boxes.length * this.state.boxes[0].length ) {
+      return `Current Player: ${this.state.currentPlayer}`;
+    }
+    else if ( this.state.scores["A"] > this.state.scores["B"] ) {
+      return `Game Over. Winner: Player A!`;
+    }
+    else if ( this.state.scores["B"] > this.state.scores["A"] ) {
+      return `Game Over. Winner: Player B!`;
+    }
+    else
+      return `Game Over. Tie Game.`
+  }
 
+  render() {
     return (
       <div className='board'>
         <div className="status">
-          Current Player: {this.state.currentPlayer}<br />
+          { this.gameStatus() }<br /><br />
           Player A: {this.state.scores['A']}<br />
           Player B: {this.state.scores['B']}
         </div>
@@ -82,8 +89,13 @@ class Board extends React.Component {
 
   checkWinner( box ) {
     this.setState( (state) => {
+      // Check if current player won the current box
       if ( box.top && box.bottom && box.left && box.right  ) {
-        state.scores[this.state.currentPlayer]++;
+        state.scores[state.currentPlayer]++;
+      }
+      // Check for game over
+      if ( state.scores["A"] + state.scores["B"] >= state.boxes.length * state.boxes[0].length ) {
+        state.currentPlayer = "";
       }
       return state;        
     });
@@ -94,6 +106,8 @@ class Board extends React.Component {
   }
 
   handleClick( e, i, j ) {
+    e.preventDefault();
+
     // First, get coordinates of click relative to box
     let x = e.clientX - e.target.getBoundingClientRect().left;
     let y = e.clientY - e.target.getBoundingClientRect().top;
@@ -101,12 +115,6 @@ class Board extends React.Component {
     // Clone boxes state
     let newBoxes = this.state.boxes.slice();
     let winnerFlag = false, turnFlag = false;
-
-    // let newBoxes = [];
-    // for ( let k=0; k<this.state.boxes.length; k++ ) {
-    //   newBoxes[k] = this.state.boxes[k].slice();
-    // }
-
 
     if ( y < 10 ) {
       if ( ! newBoxes[i][j].top ) {
@@ -155,8 +163,10 @@ class Board extends React.Component {
 
 
     // Check if current box is winner
-    newBoxes[i][j].winner = this.checkWinner( newBoxes[i][j] );
-    if ( newBoxes[i][j].winner ) winnerFlag = true;
+    if ( ! newBoxes[i][j].winner ) {
+      newBoxes[i][j].winner = this.checkWinner( newBoxes[i][j] );
+      if ( newBoxes[i][j].winner ) winnerFlag = true;
+    }
 
     // Update current player
 
@@ -174,27 +184,16 @@ class Board extends React.Component {
 }
 
 
-class Game extends React.Component {
+const Game = () =>
+  <div className="game">
+    <div className="game-board">
+      <Board height='3' width='5' />
+    </div>
+    <div className="game-info">
+      <div></div>
+    </div>
+  </div>
 
-  constructor( props )
-  {
-    super( props );
-    this.currentPlayer = 1;
-  }
-
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board height='5' width='10' />
-        </div>
-        <div className="game-info">
-          <div></div>
-        </div>
-      </div>
-    );
-  }
-}
 
 // ========================================
 
