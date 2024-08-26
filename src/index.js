3b83719e3a4d0f8c6fcda6c2092171b96e4f682a
Jesse Fischer <fischer@work.co>
2024-08-26 14:13:14 -0400

    chore: set up Ably

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Ably from "ably";
+import { AblyProvider } from "ably/react";
 
 import { App } from "./App";
 
@@ -8,8 +10,15 @@ import "./index.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const client = new Ably.Realtime({
+  key: process.env.REACT_APP_ABLY_API_KEY,
+  clientId: "<client-ID>",
+});
+
 root.render(
   <StrictMode>
-    <App />
+    <AblyProvider client={client}>
+      <App />
+    </AblyProvider>
   </StrictMode>
 );
