87ca15c6afe445f1b898388d63c2ac59096f963a
Jesse Fischer <fischer@work.co>
2024-08-26 13:27:50 -0400

    chore: add classnames

--- src/Box.js
+++ src/Box.js
@@ -1,21 +1,26 @@
+import classNames from "classnames";
+
 import styles from "./Box.module.css";
 
-export const Box = ({ top, bottom, left, right, winner, onClick }) => (
-  <div
-    className={
-      styles.box +
-      (top ? " " + styles["top" + top] : "") +
-      (bottom ? " " + styles["bottom" + bottom] : "") +
-      (left ? " " + styles["left" + left] : "") +
-      (right ? " " + styles["right" + right] : "") +
-      (winner ? " " + styles["winner" + winner] : "")
-    }
-    onClick={onClick}
-  >
-    <span className={styles.winner}>{winner || "_"}</span>
-    <div className={styles.topLeft}></div>
-    <div className={styles.topRight}></div>
-    <div className={styles.bottomLeft}></div>
-    <div className={styles.bottomRight}></div>
-  </div>
-);
+const cx = classNames.bind(styles);
+
+export const Box = ({ top, bottom, left, right, winner, onClick }) => {
+  const boxClassName = cx(
+    styles.box,
+    top && styles["top" + top],
+    bottom && styles["bottom" + bottom],
+    left && styles["left" + left],
+    right && styles["right" + right],
+    winner && styles["winner" + winner]
+  );
+
+  return (
+    <div className={boxClassName} onClick={onClick}>
+      <span className={styles.winner}>{winner || "_"}</span>
+      <div className={styles.topLeft}></div>
+      <div className={styles.topRight}></div>
+      <div className={styles.bottomLeft}></div>
+      <div className={styles.bottomRight}></div>
+    </div>
+  );
+};
